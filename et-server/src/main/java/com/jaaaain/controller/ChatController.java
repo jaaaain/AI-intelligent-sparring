72359001db503dc1d6bd50c5aa7200c3b23079c4
jiajiajiajia-000 <1079505600@qq.com>
2024-09-29 10:59:13 +0800

    小整理

---
+++ et-server/src/main/java/com/jaaaain/controller/ChatController.java
@@ -0,0 +1,11 @@
+package com.jaaaain.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+
+/**
+ * 聊天控制层，包括根据用户ID查询所有
+ */
+@RestController
+public class ChatController {
+}
--- et-server/src/main/java/com/jaaaain/mapper/ChatMessageMpper.java
+++ et-server/src/main/java/com/jaaaain/mapper/ChatMessageMpper.java
@@ -3,9 +3,13 @@ package com.jaaaain.mapper;
 import com.jaaaain.entity.ChatMessage;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
 
 @Mapper
 public interface ChatMessageMpper {
     @Insert("insert into chatmessage(session_id, sender_role, content, timestamp) value (#{sessionId},#{senderRole},#{content},NOW())")
     void insert(ChatMessage chatMessage);
+
+    @Select("select (session_id, sender_role, content, timestamp) from chatmessage where session_id=#{sessionId}")
+    void selectBySessionId(String sessionId);
 }
